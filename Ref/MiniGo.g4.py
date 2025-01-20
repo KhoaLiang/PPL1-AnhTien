465dceefe5d513404b232d584e50bda2b9dc9de9
KhoaLiang <akhoa2020@gmail.com>
2025-01-20 21:40:22 +0700

    I am going to do something rash

--- Ref/MiniGo.g4.py
+++ Ref/MiniGo.g4.py
@@ -26,8 +26,49 @@ options{
 
 //program: 'votien'+ EOF;
 // ! ---------------- PASER DEADLINE PASS 9 TEST CASE 23:59 19/1 ----------------------- */
-program: ((CONST ID ASSIGN expression) | NEWLINE)+ EOF;
+//program: ((CONST ID ASSIGN expression) | NEWLINE)+ EOF;
+//TODO declared
+program: NEWLINE* declared (declared | NEWLINE)* EOF;
+//TODO Variables 5.1 pdf
+declared:
+	variables_declared
+	| constants_declared
+	| function_declared
+	| method_declared
+	| struct_declared
+	| interface_declared;
 
+// Variable declare
+variables_declared: (implicit_var | keyword_var) SEMICOL; 
+//TODO implicit_var, keyword_var
+implicit_var: VAR ID ASSIGN expression;
+keyword_var: VAR ( primitive_declaration |  array_declaration | interface_type) (ASSIGN expression)?;
+//type of variable
+
+primitive_type: INT | FLOAT | BOOLEAN | STRING;
+primitive_declaration: ID primitive_type;
+interface_type: ID (STR | ID) ; //struct or interface representation
+dimension_list: LBRACK INT_LIT RBRACK (LBRACK INT_LIT RBRACK)*;
+array_declaration: dimension_list (primitive_type | ID); // array_declaration  view ID as the type of struct or interface
+
+//Constant declare
+constants_declared: CONST ID ASSIGN (expression | array_declaration expression) SEMICOL;
+
+// function declare
+function_declared: FUNC ID LPAREN (prameters_list)? RPAREN (primitive_type | ID | array_declaration)? LBRACE list_statement* ignore* RBRACE; //(ignore? return_statement | ignore? block_statement | ignore);
+
+// method declare
+//(ID1 ID2) --> ID1 represent the name of the struct or interface instance, ID2 represent the name of the struct or interface for example: func (c Calculator) VoTien(x int) int {}
+method_declared: FUNC LPAREN (ID ID) RPAREN ID LPAREN (prameters_list)? RPAREN (primitive_type | ID | array_declaration)? LBRACE RBRACE; //(ignore? return_statement | ignore? block_statement | ignore); 
+
+// struct declare
+struct_declared: TYPE ID STRUCT LBRACE ignore* ( prameter SEMICOL (ignore)*)* RBRACE;
+// struct declare
+interface_declared: TYPE ID INTERFACE LBRACE ignore* ( ID LPAREN (prameters_list)? RPAREN (primitive_type | ID | array_declaration)? SEMICOL? (ignore)*)* RBRACE;
+
+//TODO prameters_list
+prameters_list: prameter COMMA prameters_list | prameter; 
+prameter: primitive_declaration | (ID array_declaration) | ID;
 //TODO Literal 6.6 pdf
 literal:
     INT_LIT
@@ -38,15 +79,14 @@ literal:
     | array_literal
     | struct_literal;
 
-array_literal: LBRACE list_expression RBRACE;
+array_literal: LBRACE params RBRACE;
 struct_literal: ID LBRACE list_elements RBRACE;
 
-type_of_array: INT | FLOAT | BOOLEAN | STRING | STRUCT;
-dimension_list: LBRACK INT_LIT RBRACK (LBRACK INT_LIT RBRACK)*;
-type_array: dimension_list type_of_array;
+
 
 //TODO Expression 6 pdf
-list_expression: expression (COMMA expression)*;
+list_expression: params | ; // list này có thể rỗng
+params: expression COMMA params | expression; //params thì không -> làm param của index trong mảng
 
 list_elements: (ID COLON expression) (COMMA ID COLON expression)*;
 
@@ -56,14 +96,55 @@ expression2: expression2 EQUAL expression3 | expression2 DIFF expression3 | expr
 expression3: expression3 ADD expression4 | expression3 SUB expression4 | expression4;
 expression4: expression4 MUL expression5 | expression4 DIV expression5 | expression4 MOD expression5 | expression5;
 expression5: NOT expression5 | SUB expression5 | expression6;
-expression6: type_array array_literal | expression6 POINTTO expression7 | expression7;
+expression6: expression6 (LPAREN list_expression RPAREN)? LBRACK (params) RBRACK | expression6 POINTTO expression7 | expression7;
 
 // expression6: LBRACK expression6 RBRACK (LBRACK expression6 RBRACK)* type_of_array? | expression6 POINTTO expression7 | expression7;
-expression7: LPAREN expression RPAREN | ID (LBRACK INT_LIT RBRACK)? | literal (LBRACK INT_LIT RBRACK)? | func_call (LBRACK INT_LIT RBRACK)? |;
+expression7: LPAREN expression RPAREN | ID  | literal  | func_call;
 func_call: ID LPAREN list_expression RPAREN;
 
+// kí tự bỏ qua
+ignore: NEWLINE+;
 
+//TODO Statement 5 and 4 pdf
+list_statement: statement list_statement | statement;
+statement:
+	(
+		declared_statement
+	    | assign_statement
+		| if_statement
+		| for_statement
+		| break_statement
+		| continue_statement
+		| return_statement
+        | call_statement
+	);
+declared_statement: ignore* (variables_declared | constants_declared) ignore*;
 
+//assign_statement
+assign_statement: ignore* (ID POINTTO? ID? (LBRACK INT_LIT RBRACK)*) assignment_operator expression SEMICOL ignore*;
+assignment_operator: ASSIGN | ASSIGNADD | ASSIGNSUB | ASSIGNMUL | ASSIGNDIV | ASSIGNMOD | ASSIGNNIT;
+
+//if_statement
+if_statement: ignore* IF LPAREN expression RPAREN  (lbrace_code_block) list_elif (ELSE (LBRACE ignore* statement RBRACE))?;
+list_elif: ignore* ELSE IF LPAREN expression RPAREN (LBRACE statement RBRACE) list_elif | ;
+
+//for_statement
+
+for_statement: basic_for | init_condition_update_for | range_for;
+basic_for: ignore* FOR expression ignore* (lbrace_code_block);
+init_condition_update_for: ignore* FOR (ID assignment_operator INT_LIT) SEMICOL (expression) SEMICOL (ID assignment_operator INT_LIT) ignore* (lbrace_code_block);
+range_for: ignore* FOR ID COMMA ID ASSIGNNIT RANGE ID ignore* (lbrace_code_block);
+
+//break_statement
+break_statement: ignore* BREAK SEMICOL ignore*;
+//continue_statement
+continue_statement: ignore* CONTINUE SEMICOL ignore*;
+//return_statement
+return_statement: ignore* RETURN (expression)? SEMICOL? ignore*;
+//call_statement
+call_statement: ignore* expression SEMICOL ignore*;
+//inside braces for function, if, for
+lbrace_code_block: LBRACE (statement*) ignore* RBRACE;
 //! ---------------- PASER ----------------------- */
 
 
@@ -91,6 +172,8 @@ NIL: 'nil';
 TRUE: 'true';
 FALSE: 'false';
 
+STR: 'str';
+
 //TODO Operators 3.3.3 pdf
 ADD: '+';
 SUB: '-';
@@ -114,6 +197,7 @@ ASSIGNDIV: '/=';
 ASSIGNMOD: '%=';
 POINTTO: '.';
 COLON: ':';
+ASSIGNNIT: ':='; //(same as <-)
 
 //TODO Separators 3.3.4 pdf
 LPAREN: '(';
@@ -171,6 +255,7 @@ fragment ESC_ILLEGAL: '\\' ~[ntr"\\];
 //TODO skip 3.1 and 3.2 pdf
 WS: [ \t\f\r]+ -> skip; // skip spaces, tabs, form feeds, and carriage returns
 NEWLINE: '\r'? '\n';
+
 //SINGLE LINE COMMENT
 COMMENT: '//' ~[\r\n]* -> skip;
 
